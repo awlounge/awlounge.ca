ea5960bd16e9ffc74f8833ceb0b5ca610f818a68
awlounge <awl.jm2@gmail.com>
2025-09-29 21:58:05 -0400

    Fixing outgoing email bug...

--- server.js
+++ server.js
@@ -145,10 +145,12 @@ const auth = new google.auth.GoogleAuth({
 const calendar = google.calendar({ version: "v3", auth });
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // Use SSL
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASS, // Your 16-character App Password
     },
 });
 
